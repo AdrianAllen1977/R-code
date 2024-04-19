0ee73a80a6e5cdbb029223046ea4d71d7d5640d8
AdrianAllen1977 <47992099+AdrianAllen1977@users.noreply.github.com>
2024-04-19 11:25:00 +0100

    Add files via upload

--- iMAGE_app.r
+++ iMAGE_app.r
@@ -5,6 +5,10 @@ library(shinyjs)
 library(dplyr)
 library(DT)
 library(ape)
+library(ggplot2)
+library(ggdensity)
+library(rgdal)
+
 
 ### Import distance matrix, phylogeny and location data files.
 
@@ -12,8 +16,10 @@ snp_distance_matrix <- read.csv("snp_distance_matrix.csv", header = TRUE, string
 
 location_data <- read.csv("location_data.csv", header = TRUE,  stringsAsFactors = FALSE)
 
-tree<-read.tree("All_bovis.tree")
+tree<-read.tree("All_bovis2.tree")
 
+# Make branches with lengths of 0 very very small - RaxML trees can appear very different, ie not sitting flat to node, when zoomed in on samples that are identical - feature of substitutions per site being the metric used.
+tree$edge.length[tree$edge.length <= 0] <- 0.000001
 
 ## Make sure Lat and Lon data are numeric
 location_data$Lat<-as.numeric(location_data$Lat)
@@ -33,9 +39,9 @@ ui <- fluidPage(
     tabPanel("i-MAGE",
              titlePanel("i-MAGE: Interactive MycobActerium bovis Genome Epidemiology"),
              mainPanel(
-               img(src="iMage_logo.png", align="centre", width="150%"), ## Your image must be in a subfolder called 'www' or else it won't display
+               img(src="iMage_logo_2.png", align="centre", width="100%"), ## Your image must be in a subfolder called 'www' or else it won't display
                img(src="AFBI_EMbI_logo.png", align="right", width="50%"),
-               img(src="sigil.png", align="left", width="3%")
+               img(src="sigil.png", align="left", width="1%")
                )
     ),
     
@@ -53,10 +59,17 @@ ui <- fluidPage(
                        p("6. You can then go to the Phylogeny tab, to view the highlighted chosen sample's phylogenetic relationship to the closest relatives defined by the chosen SNP distance from the Map tab."),
                        p("7. If the SNP threshold is high, it may pull in so many samples that the phylogeny plot resolution is poor."),
                        p("8. Use the slider to dynamically adjust the height of the phylogeny plot accordingly."),
+                       p(strong("The 'Home/Core Range Finder' Tab")),
+                       p("9. You can go to the Home / Core range which displays the 50% and 95% kernel density estimate for the subset of samples chosen by the applied SNP cutoff"),
+                       p("10. The 95% Home Range is minimum area over which 95% of the most densely clustered isolates are found and is coloured light blue."),
+                       p("11. The 50% Core Range is the minimum area over which 50% of of the most densely clustered isolates are found and is coloured dark blue"),
+                       p("12. The locations of the subset of isolates chosen by SNP cutoff are displayed as red dots."),
+                       p("13.The 50% Core Range is the densest area of the chosen sample subset and likely corresponds to the historical origin of the selected M. bovis lineage."),
+                       p("14. Isolates that are placed outside this core range should have their movement records checked to determine if they have a link back to the core area."),
                        p(strong("The 'Follow Up Investigation' Tab")),
-                       p("9. Then, go to the 'Follow Up Investigation' tab where the closest relatives of your isolate will be listed, and the herd in which they have been found."),
-                       p("10. The format of the isolate name will tell you the host species (bov=bovine, bad=badger) and the year of isolation"),
-                       p("11. If some of the closest isolates are from wildlife sources, there will be no herd ID entry"),
+                       p("15. Then, go to the 'Follow Up Investigation' tab where the closest relatives of your isolate will be listed, and the herd in which they have been found."),
+                       p("16. The format of the isolate name will tell you the host species (bov=bovine, bad=badger) and the year of isolation"),
+                       p("17. If some of the closest isolates are from wildlife sources, there will be no herd ID entry"),
                        p(strong("CAVEAT! M. bovis does not mutate very frequently, so in outbreak areas there may be little diversity.")),
                        p(strong("A close match to another herd does NOT necessarily mean that is where infection came from, indeed many isolates could have a similar level of relatedness or be identical to the one you are interested in.")),
                        p(strong("OR the true source may not have been sampled, while others nearby have.")),
@@ -88,7 +101,13 @@ ui <- fluidPage(
                 )
     ),
     
-    
+    # Add the new tab for the Core Range viewer
+    tabPanel("Home / Core Range Finder",
+             titlePanel("Core range of selected SNP distance isolates"),
+             mainPanel(
+               plotOutput("shapefile_map")
+             )
+    ), 
     # Tab for the derived information
     tabPanel("Follow Up Investigation",
              titlePanel("Check movement & land parcel data for these locations"),
@@ -97,6 +116,8 @@ ui <- fluidPage(
                dataTableOutput("derived_table")
              )
     ),
+    
+
     
     ## Tab for the acknowledgements panel
     tabPanel("Acknowledgements", 
@@ -155,7 +176,7 @@ filtered_matrix()[,1]
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      setView(lng = -6.9, lat = 54.6, zoom = 8) %>%
+      setView(lng = -6.9, lat = 54.6, zoom = 6) %>%
       addMarkers(
         data = other_sample_locs(),
         lng = ~Lon,
@@ -164,10 +185,11 @@ filtered_matrix()[,1]
         labelOptions = labelOptions(
           direction = "auto",                      # Automatically choose label direction
           noHide = F,                           # Keep the label open on hover
-          style = list("border-color" = "white", "background-color" = "white")  # Style for the label
+          style = list("border-color" = "blue", "background-color" = "white")  # Style for the label
         ),
-        clusterOptions = markerClusterOptions()  # Optional: Add marker clustering
+        clusterOptions = markerClusterOptions(),  # Optional: Add marker clustering
       )
+    
   })
   
 
@@ -185,6 +207,15 @@ filtered_matrix()[,1]
     
     # Create the phylogeny plot
     plot(subtree, type = "phylogram", cex = 1.0, tip.color = tip_colors)
+    
+    # Get the size of the plot window in centimetres
+    plot_size <- dev.size("cm")
+    
+    # Set the x and y coordinates for adding a scale bar
+    xscale <- 0.8 * plot_size[1]
+    
+    add.scale.bar(x=xscale, y=-1, lcol="blue", lwd=3, length=2)
+    
   })
   
   # Render the phylogeny plot
@@ -194,13 +225,31 @@ filtered_matrix()[,1]
     input$phylogeny_height
   })
  
+  # Load the core range shapefile
+  NI <- readOGR(dsn = ".", layer = "NI2_outline_small")
+  NI_df <- fortify(NI)
+  
+  ## Render the map with filtered locations and kernel density plot
+  output$shapefile_map <- renderPlot({
+    # print(NI_df)  # Check if NI_df contains data
+    #print(other_sample_locs())  # Check if other_sample_locs() contains data
     
+    ggplot() +
+      geom_polygon(data = NI_df, aes(x = long, y = lat, group = group), colour = "black", fill = "white") +
+      geom_hdr(data = other_sample_locs(), aes(x = Lon, y = Lat), method="kde", probs=c(0.95, 0.50), fill="blue") +
+      geom_point(data = other_sample_locs(), aes(x = Lon, y = Lat), color = "red", size = 1) +
+      theme_bw()
+  })  
+  
+  
+  
   # Render the derived information on the fourth page/tab
   output$derived_table <- renderDataTable({
     selected_data <-other_sample_locs()[,c(1,4,5,6)]
     datatable(selected_data)
   })
   
+
   
   
 observe({
